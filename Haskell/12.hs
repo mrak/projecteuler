06ee2f78517df05765a26f8107445cd8b54d62fd
Eric Mrak <enmrak@gmail.com>
2013-05-01 19:20:16 +0530

    formatting for 12 in haskell

--- Haskell/12.hs
+++ Haskell/12.hs
@@ -1,11 +1,11 @@
 triangleNumbers :: [Integer]
 triangleNumbers = [ sumUpTo x | x <- [1..] ]
-   where sumUpTo = sum . flip take [0..]
+   where sumUpTo = sum.flip take [0..]
 
 divisors :: Integral a => a -> [a]
-divisors x = foldr accumulateDivisors [] upToSqrt
+divisors x = foldr accumulateDivisors [] [1..limit]
    where accumulateDivisors = (\n acc -> if x `mod` n == 0 then n:(quot x n):acc else acc)
-         upToSqrt = [1..((floor . sqrt . fromIntegral) x)]
+         limit = (floor.sqrt.fromIntegral) x
 
 main = print $ head [ x | x <- triangleNumbers, (numDivisors x) > 500 ]
-   where numDivisors x = length $ divisors x
+   where numDivisors = (length.divisors)
