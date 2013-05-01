974a69d9258a84e87f73ca03d3db8e7f5a2443c7
Eric Mrak <enmrak@gmail.com>
2013-05-01 19:14:24 +0530

    finished problem 12!

--- Haskell/12.hs
+++ Haskell/12.hs
@@ -1,7 +1,11 @@
+triangleNumbers :: [Integer]
 triangleNumbers = [ sumUpTo x | x <- [1..] ]
    where sumUpTo = sum . flip take [0..]
 
-divisors x = [ n | n <- [1..((floor . sqrt . fromIntegral) x)], x `mod` n == 0 ]
+divisors :: Integral a => a -> [a]
+divisors x = foldr accumulateDivisors [] upToSqrt
+   where accumulateDivisors = (\n acc -> if x `mod` n == 0 then n:(quot x n):acc else acc)
+         upToSqrt = [1..((floor . sqrt . fromIntegral) x)]
 
 main = print $ head [ x | x <- triangleNumbers, (numDivisors x) > 500 ]
    where numDivisors x = length $ divisors x
